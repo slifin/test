04b4ff9a7f4d429e16ba22fd953b9a761863d8a8
Adrian Smith <sfyire@gmail.com>
2019-03-23 16:33:55 +0000

    Change spacing in transducers.php

--- src/transducers.php
+++ src/transducers.php
@@ -2,18 +2,25 @@
 
 namespace slifin\test\transducers;
 
+/**
+ * A transducer to retrieve unique values.
+ *
+ * @return \Closure Function that returns a transducer.
+ */
 function unique() : \Closure
 {
     return function (array $xf) : array {
         $outer = [];
         return [
-            'init'   => $xf['init'],
+            'init' => $xf['init'],
             'result' => $xf['result'],
-            'step'   => function ($result, $input) use ($xf, &$outer) {
+            'step' => function ($result, $input) use ($xf, &$outer) {
+
                 if (!in_array($input, $outer)) {
                     $outer[] = $input;
                     return $xf['step']($result, $input);
                 }
+
                 return $result;
             }
         ];
